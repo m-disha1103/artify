0a2ee8b1154e5e905be20c95d3dbcb98d6912cf1
m-disha1103 <disham492@gmail.com>
2026-03-24 21:17:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# artify
+canva-like site
---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// simple test drawing
+ctx.fillStyle = "yellow";
+ctx.fillRect(50, 50, 150, 100);
